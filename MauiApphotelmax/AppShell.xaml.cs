4dc1270d221d5693847d7497901bd3619cf54ca5
maxwelldoti <maxwell.lima@fatec.sp.gov.br>
2024-11-01 13:02:29 -0300

    Adicionar arquivos de projeto.

---
+++ MauiApphotelmax/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiApphotelmax
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiApphotelmax/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiApphotelmax
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
