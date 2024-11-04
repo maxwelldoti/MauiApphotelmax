71c1d112c77b6f356f0b8ee6348003d486dda61b
maxwelldoti <maxwell.lima@fatec.sp.gov.br>
2024-11-04 15:16:39 -0300

    ok

--- MauiApphotelmax/App.xaml.cs
+++ MauiApphotelmax/App.xaml.cs
@@ -1,7 +1,37 @@
-﻿namespace MauiApphotelmax
+﻿using MauiApphotelmax.Models;
+
+namespace MauiApphotelmax
 {
     public partial class App : Application
     {
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Suíte Super Luxo",
+                ValorDiariaAdulto = 110.0,
+                ValorDiariaCrianca = 55.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Luxo",
+                ValorDiariaAdulto = 80.0,
+                ValorDiariaCrianca = 40.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Single",
+                ValorDiariaAdulto = 50,
+                ValorDiariaCrianca = 25
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Crise",
+                ValorDiariaAdulto = 25,
+                ValorDiariaCrianca = 12.5
+            }
+        };
+
         public App()
         {
             InitializeComponent();
@@ -9,14 +39,14 @@
             MainPage = new NavigationPage(new Views.ContratacaoHospedagem());
         }
 
-            protected override Window CreateWindow(IActivationState? activationState) 
-            {
-                  var window = base.CreateWindow(activationState);
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
 
-                  window.Width = 400;
-                  window.Height = 600;
-                  
+            window.Width = 400;
+            window.Height = 600;
+
             return window;
-            }
+        }
     }
 }
--- MauiApphotelmax/Views/ContratacaoHospedagem.xaml.cs
+++ MauiApphotelmax/Views/ContratacaoHospedagem.xaml.cs
@@ -4,9 +4,21 @@ namespace MauiApphotelmax.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
 {
+    App PropriedadesApp;
+
     public ContratacaoHospedagem()
     {
         InitializeComponent();
+
+        PropriedadesApp = (App)Application.Current;
+
+        pck_quarto.ItemsSource = PropriedadesApp.lista_quartos;
+
+        dtpck_checkin.MinimumDate = DateTime.Now;
+        dtpck_checkin.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, DateTime.Now.Day);
+
+        dtpck_checkout.MinimumDate = dtpck_checkin.Date.AddDays(1);
+        dtpck_checkout.MaximumDate = dtpck_checkin.Date.AddMonths(6);
     }
 
     private void Button_Clicked(object sender, EventArgs e)
