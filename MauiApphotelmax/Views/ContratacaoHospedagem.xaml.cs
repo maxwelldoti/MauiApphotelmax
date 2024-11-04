3498f6b8dfc906a2bcbb00b9cd2cc9eee36fd7af
maxwelldoti <maxwell.lima@fatec.sp.gov.br>
2024-11-04 14:29:27 -0300

    ok

--- MauiApphotelmax/Views/ContratacaoHospedagem.xaml.cs
+++ MauiApphotelmax/Views/ContratacaoHospedagem.xaml.cs
@@ -1,15 +1,28 @@
+using System.Linq.Expressions;
+
 namespace MauiApphotelmax.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
 {
-	public ContratacaoHospedagem()
-	{
-		InitializeComponent();
-	}
+    public ContratacaoHospedagem()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Navigation.PushAsync(new HospedagemContratada());
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        } 
+    }
 
     private async void OnSobreButtonClicked(object sender, EventArgs e)
-	{
-
+    {
         await DisplayAlert("Sobre", "Desenvolvedor: Max Almeida\nAno de Desenvolvimento: 2024", "OK");
     }
 }
